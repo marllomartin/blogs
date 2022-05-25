4f1339850dec3bae387c0c9374a82e6520a1c25c
marllomartin <marllon3636@gmail.com>
2022-05-25 19:12:43 -0300

    feat(src/server.js): Implementa rota POST de Login

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const app = require('./api');
 
+const { login } = require('./controllers/userController');
+
+const authLogin = require('./middlewares/authLogin');
+
 // não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
 
@@ -8,5 +12,8 @@ const port = process.env.API_PORT || 3000;
 app.get('/', (_request, response) => {
   response.send();
 });
+
+// Login
+app.post('/login', authLogin, login);
 
 app.listen(port, () => console.log('ouvindo porta', port));
