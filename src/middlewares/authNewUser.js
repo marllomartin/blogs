1bb70eef6c4d278f7bbce28c26bcc456b76a50f5
marllomartin <marllon3636@gmail.com>
2022-05-27 20:28:16 -0300

    feat(src/middlewares): Cria middleware de Criar novo Usuário - Middleware de validação dos campos 'displayName', 'email' e 'password'

---
+++ src/middlewares/authNewUser.js
@@ -0,0 +1,24 @@
+const authNewUser = (req, res, next) => {
+  const { displayName, email, password } = req.body;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (displayName.length < 8) {
+    return res.status(400).json({
+      message: '"displayName" length must be at least 8 characters long',
+    });
+  }
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({
+      message: '"email" must be a valid email',
+    });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({
+      message: '"password" length must be at least 6 characters long',
+    });
+  }
+
+  return next();
+};
+
+module.exports = authNewUser;
