9467ca4a2e601e0cd7362bf182fc7d73030321f1
marllomartin <marllon3636@gmail.com>
2022-05-29 17:12:28 -0300

    feat(src/middlewares): Cria middleware de Criar nova Categoria - Middleware de validação do campo 'name'

---
+++ src/middlewares/authNewCategory.js
@@ -0,0 +1,13 @@
+const authNewCategory = (req, res, next) => {
+  const { name } = req.body;
+
+  if (!name || name.length === 0) {
+    return res.status(400).json({
+      message: '"name" is required',
+    });
+  }
+
+  return next();
+};
+
+module.exports = authNewCategory;
