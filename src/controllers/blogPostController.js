63bfe6cdaea8f1da03eb94cbbd8132d5a67045ee
marllomartin <marllon3636@gmail.com>
2022-05-30 18:53:44 -0300

    feat(src/controllers): Cria controller de Buscar Todos os Posts

---
+++ src/controllers/blogPostController.js
@@ -0,0 +1,12 @@
+const blogPostService = require('../services/blogPostService');
+
+const getAllBlogPosts = async (req, res) => {
+  try {
+    const result = await blogPostService.getAllBlogPosts();
+    return res.status(200).json(result);
+  } catch (Error) {
+    return res.status(404).send({ message: Error.message });
+  }
+};
+
+module.exports = { getAllBlogPosts };
